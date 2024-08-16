4c145d5e95569103c3397ae9ae57fc4d33c0cd98
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2024-08-16 23:57:39 +0300

    Added digital clock program and started stopwatch program

--- index.js
+++ index.js
@@ -1734,6 +1734,7 @@ console.log(`The final score is ${game.getScore()}pts`);
 //Not for precise stuff (like stopwatch)
 //setTimeout(callback, delay);
 //clearTimeout(timeoutId) can cancel a timeout before it triggers
+/*
 function sayHello(){
     window.alert("Hello");
 }
@@ -1753,4 +1754,56 @@ function startTimer(){
 function clearTimer(){
     clearTimeout(timeoutId);
     console.log("Cleared the timer");
+}
+*/
+//DIGITAL CLOCK PROGRAM
+//sale.html file involved
+function updateClock() {
+    const now = new Date(); //the current date is stored within this object
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "p.m." : "e.m.";
+    hours = hours % 12 || 12; //"|| 12" means that if the remainder equals 0, then 12 will be showed instead
+    hours = hours.toString().padStart(2, 0); //.toString() returns a string representation of an object, and .padStart(), as was previously learned, sets 2 characters for hours and it starts with "0", if only one character's used
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds}${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
+}
+updateClock();
+setInterval(updateClock, 1000); //will call the function repeatedly, where 1000 ms means every second
+//STOPWATCH PROGRAM
+const display = document.getElementById("display");
+let timer = null; //"null" means no value
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start() {
+    if(!isRunning) { //if it's false
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10); //every 10 ms will be updated
+        isRunning = true;
+    }
+}
+function stop() {
+
+}
+function reset() {
+
+}
+function update() {
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime; //in milliseconds
+
+    let hours = Math.trunc(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.trunc(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.trunc(elapsedTime / 1000 % 60);
+    let milliseconds = Math.trunc(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, 0); //same as in the previous exercise
+    minutes = String(minutes).padStart(2, 0);
+    seconds = String(seconds).padStart(2, 0);
+    milliseconds = String(milliseconds).padStart(2, 0);
+    
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
