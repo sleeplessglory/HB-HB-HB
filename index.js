8f607a254241ae08e0f779c36370d463d6cb45e5
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2024-08-18 23:34:15 +0300

    Finished DOM navigation, added adding and changing HTML

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//IF ANYTHING DOES NOT WORK, CONSIDER COMMENTING PARTS OF THE EXERCISES (MADE ALL TOGETHER)
 //BASICS OF JAVASCRIPT
 /*
 console.log(`Hello`); //We can use ` `, ' ' or " "
@@ -2014,6 +2015,94 @@ fruits.forEach(fruit => {
 //.previousElementSibling
 //.parentElement
 //.children
-const element = document.getElementById("fruits"); //reference to this unordered list
+/*
+let element = document.getElementById("fruits"); //reference to this unordered list
 const firstChild = element.firstElementChild;
-firstChild.style.backgroundColor = "lightgreen";
+firstChild.style.backgroundColor = "lightgreen"; //highlights the "Apple"
+
+let ulElements = document.querySelectorAll("ul");
+ulElements.forEach(ulElement => {
+    const firstChild = ulElement.firstElementChild; //first child of every <ul> element
+    firstChild.style.backgroundColor = "yellow";
+});
+
+
+element = document.getElementById("desserts");
+const lastChild = element.lastElementChild;
+lastChild.style.backgroundColor = "lightblue";
+
+ulElements = document.querySelectorAll("ul");
+ulElements.forEach(ulElement => {
+    const lastChild = ulElement.lastElementChild;
+    lastChild.style.backgroundColor = "lightblue";
+});
+
+
+element = document.getElementById("apple"); //if we choose "fruits" ID, it's gonna apply to the whole next (sibling) element ("vegetables")
+nextSibling = element.nextElementSibling;
+nextSibling.style.fontStyle = "italic"; //"orange" is now italic
+
+
+element = document.getElementById("vegetables"); //the previous sibling is "fruits"
+const prevSibling = element.previousElementSibling;
+prevSibling.style.fontWeight = "bold";
+
+
+element = document.getElementById("cake");
+const parent = element.parentElement;
+parent.style.fontStyle = "italic";
+
+
+element = document.getElementById("vegetables");
+const children = element.children; //returns an HTML collection
+Array.from(children).forEach(child => { //we need to turn this collection into an array and go through each element
+    child.style.fontSize = "1.4em";
+});
+//or we can access this HTML collection by an index number
+children[1].style.backgroundColor = "pink"; //the 2nd child is highlighted now
+*/
+//ADD AND CHANGE HTML
+/*
+//1. Create the element
+const newH1 = document.createElement("h1"); //specify the tag
+//2. Add attributes/properties
+newH1.textContent = "I like pizza!";
+newH1.id = "newID"; //added an "id" attribute for it with the "newID" value
+newH1.style.color="red"; //added a CSS property
+newH1.style.textAlign = "center";
+//3. Append element to DOM
+//document.body.append(newH1); //the last child of <body> parent
+//document.body.prepend(newH1); //the 1st child of <body> parent
+document.getElementById("box2-id").append(newH1); //the last child of the 2nd box
+document.getElementById("box3-id").prepend(newH1); //the 1st child of the 3rd box
+
+const box2 = document.getElementById("box2-id");
+document.body.insertBefore(newH1, box2); //.insertBefore(newElement, currentElement)
+//Let's pretend that these boxes don't have IDs, then:
+const boxes = document.querySelectorAll("box-class"); //nodelist is assigned to the variable
+//document.body.insertBefore(newH1, boxes[1]); //before the 2nd box
+
+//4. Remove the element
+//document.body.removeChild(newH1); //if the <body> is parent
+document.getElementById("box3-id").removeChild(newH1); //if this box is the parent
+
+
+//1. Create the element
+let newListItem = document.createElement("li");
+//2. Add attributes/properties
+newListItem.textContent = "Potatoes and mushrooms";
+newListItem.id = "potatoes-mushrooms";
+newListItem.style.fontWeight = "bold";
+newListItem.style.backgroundColor = "lightgreen";
+//3. Append element to DOM
+document.body.append(newListItem);
+document.getElementById("lunch").append(newListItem);
+document.getElementById("lunch").prepend(newListItem);
+const spaghetti = document.getElementById("spaghetti");
+document.getElementById("lunch").insertBefore(newListItem, spaghetti);
+//Let's pretend that lunch items do not have IDs, then:
+const listItems = document.querySelectorAll("#lunch li"); //selects all the lists within the "lunch" ID
+document.getElementById("lunch").insertBefore(newListItem, listItems[4]); //listItems is a nodelist (specify the index in order to apply anything)
+//4. Remove the element
+document.getElementById("lunch").removeChild(newListItem); //removes the new element (child)
+*/
